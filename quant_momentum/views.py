bee2f56f37cb74df409b3d75446388103d499289
Prasun Sharma <prasunnyD@github.com>
2020-12-24 17:04:18 -0500

    added batch api functionality

--- quant_momentum/views.py
+++ quant_momentum/views.py
@@ -17,16 +17,23 @@ def home(request):
 	# 	symbols = list(reader)
 
 	my_columns = ['Ticker', 'Price']
+	symbol_groups = list(chunks(stocks['Ticker'], 100))
+	symbol_strings = []
+	for i in range(0, len(symbol_groups)):
+		symbol_strings.append(','.join(symbol_groups[i]))
+	
 	final_dataframe = pd.DataFrame(columns = my_columns)
 	price_list = []
-	for symbol in stocks['Ticker']:
-		api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={IEX_CLOUD_API_TOKEN}'
-		data = requests.get(api_url).json()
-		final_dataframe = final_dataframe.append(
-			pd.Series([symbol,
-				data['latestPrice']],
-				index = my_columns),
-			ignore_index = True)
+
+	for symbol_string in symbol_strings:
+		batch_api_url = f'https://sandbox.iexapis.com/stable/stock/market/batch/?types=quote&symbols={symbol_string}&token={IEX_CLOUD_API_TOKEN}'
+		data = requests.get(batch_api_url).json()
+		for symbol in symbol_string.split(','):
+			final_dataframe = final_dataframe.append(
+				pd.Series([symbol,
+					data[symbol]['quote']['latestPrice']],
+					index = my_columns),
+				ignore_index = True)
 
 	# return render(request, 'quant_momentum/momentum.html', {
 	# 	'dataframe': final_dataframe,
@@ -43,3 +50,8 @@ def home(request):
 	htmltable = final_dataframe.to_html()
 	return HttpResponse(htmltable)
 	
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
